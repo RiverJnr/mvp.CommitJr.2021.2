49818d070e25aa63dec258d9a02b54ed71e1bc08
RiverJnr <riverazeredo@gmail.com>
2021-11-02 18:07:03 -0300

    edit load more

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -7,9 +7,26 @@ const port = 3333;
 
 app.use(express.json());
 app.use(router);
-app.use(cors());
+//app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET, PUT, POST');
+    app.use(cors());
+    next();
+})
 
 app.listen(port, () => {
     console.log("Executando a aplicação em http://localhost:"+port);
 });
+
+
+
+
+
+
+
+
+
+
 
